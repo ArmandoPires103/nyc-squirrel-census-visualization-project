3e7029f309fa29489cbc052c6915d631deaf45f9
armandopires <armandopires@pursuit.org>
2024-05-06 13:30:46 -0400

    Added images

--- src/MostWanted.jsx
+++ src/MostWanted.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
+import mostWanted from "../mostWanted"; // Importing the array of image URLs
 
 const MostWanted = () => {
   const [approachedSquirrels, setApproachedSquirrels] = useState([]);
 
   useEffect(() => {
-    fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json")
+    fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$where=runs_from=false%20AND%20approaches=true&$limit=6")
       .then(response => response.json())
       .then(data => {
         console.log(data)
-        const filteredSquirrels = data.filter(squirrel => squirrel.approaches === "true");
-        setApproachedSquirrels(filteredSquirrels);
+        setApproachedSquirrels(data);
       })
       .catch(error => console.error("Error fetching data:", error));
   }, []); 
@@ -18,11 +18,17 @@ const MostWanted = () => {
     <div>
       <h1>The Most Wanted Squirrels</h1>
       <ul>
-        {approachedSquirrels.map(squirrel => (
+        {approachedSquirrels.map((squirrel, index) => (
           <li key={squirrel.unique_squirrel_id}>
-            {/* Render squirrel details here */}
-            {squirrel.unique_squirrel_id}
-            {squirrel.approaches}
+            {/* Render the image and squirrel details */}
+            <img src={mostWanted[index]} alt={`Squirrel ${index}`} style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+            {squirrel.unique_squirrel_id && (
+              <div>
+                <p>Unique Squirrel ID: {squirrel.unique_squirrel_id}</p>
+                <p>Approaches: {squirrel.approaches ? 'Yes' : 'No'}</p>
+                <p>Runs At you: {squirrel.runs_from ? 'No' : 'Yes'}</p>
+              </div>
+            )}
           </li>
         ))}
       </ul>
