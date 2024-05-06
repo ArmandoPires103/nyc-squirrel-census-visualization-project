cd20a9ac93fbcb54005f376ecc566c280f7a7d1b
armandopires <armandopires@pursuit.org>
2024-05-06 12:38:24 -0400

    Added-css

---
+++ squirrelImages.js
@@ -0,0 +1,14 @@
+// squirrelImages.js
+
+const squirrelImages = [
+    "https://res.cloudinary.com/dhexjuuzd/image/upload/v1714758150/FrontProfileSquirrel_2300H_e0tfol.jpg",
+    "https://res.cloudinary.com/dhexjuuzd/image/upload/v1714947566/download_4_fmxmt2.jpg",
+    "https://res.cloudinary.com/dhexjuuzd/image/upload/v1715007127/images_1_s1mzsm.jpg",
+    "https://res.cloudinary.com/dhexjuuzd/image/upload/v1715007131/images_2_s9hpbi.jpg",
+    "https://res.cloudinary.com/dhexjuuzd/image/upload/v1715007187/images_3_pekvod.jpg",
+    "https://res.cloudinary.com/dhexjuuzd/image/upload/v1715007385/images_4_pvxhp5.jpg",
+    // Add more image URLs here as needed
+  ];
+  
+  export default squirrelImages;
+  
