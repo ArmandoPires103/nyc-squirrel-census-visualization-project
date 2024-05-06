4ef33d589ead7c807c367e4a04b3c8aa76694e41
Carlitos <carlitosdutan@pursuit.org>
2024-05-06 15:57:47 -0400

    finished project

--- src/MostWanted.jsx
+++ src/MostWanted.jsx
@@ -1,32 +1,62 @@
 import React, { useState, useEffect } from "react";
 import mostWanted from "../mostWanted"; // Importing the array of image URLs
+import "../Styles/beware.css";
+import { grabName } from "../helper";
 
 const MostWanted = () => {
   const [approachedSquirrels, setApproachedSquirrels] = useState([]);
 
   useEffect(() => {
-    fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json?$where=runs_from=false%20AND%20approaches=true&$limit=6")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
+    fetch(
+      "https://data.cityofnewyork.us/resource/vfnx-vebw.json?$where=runs_from=false%20AND%20approaches=true&$limit=6"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
         setApproachedSquirrels(data);
       })
-      .catch(error => console.error("Error fetching data:", error));
-  }, []); 
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
-    <div>
-      <h1>The Most Wanted Squirrels</h1>
-      <ul>
+    <div className="mostwanted-container">
+      <h1 className="beware-title-h1">The Most Wanted Squirrels</h1>
+      <ul className="card-layout mostwanted">
         {approachedSquirrels.map((squirrel, index) => (
-          <li key={squirrel.unique_squirrel_id}>
+          <li
+            key={squirrel.unique_squirrel_id}
+            className="squirrel-card mostwanted"
+          >
+            <p className="beware-title-p">{grabName()}</p>
             {/* Render the image and squirrel details */}
-            <img src={mostWanted[index]} alt={`Squirrel ${index}`} style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+
+            <img
+              src={mostWanted[index]}
+              alt={`Squirrel ${index}`}
+              style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+            />
+
             {squirrel.unique_squirrel_id && (
-              <div>
-                <p>Unique Squirrel ID: {squirrel.unique_squirrel_id}</p>
-                <p>Approaches: {squirrel.approaches ? 'Yes' : 'No'}</p>
-                <p>Runs At you: {squirrel.runs_from ? 'No' : 'Yes'}</p>
+              <div className="mostwanted-card-container">
+                <p className="beware-title-p-1">
+                  Criminal Serial Number: {squirrel.unique_squirrel_id}
+                </p>
+                <p className="beware-title-p-2">
+                  Approaches:{" "}
+                  {squirrel.approaches ? (
+                    <span className="beware-title-span-y">Yes</span>
+                  ) : (
+                    <span className="beware-title-span-n">No</span>
+                  )}
+                </p>
+                <p className="beware-title-p-3">
+                  Runs At you:{" "}
+                  {squirrel.runs_from ? (
+                    <span className="beware-title-span-y">No</span>
+                  ) : (
+                    <span className="beware-title-span-y">Yes</span>
+                  )}
+                </p>
               </div>
             )}
           </li>
